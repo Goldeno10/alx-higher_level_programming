4ed84529caf5c7aee4aa4d53b27ff3258dedf112
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-06-30 15:39:34 +0100

    task  1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1.py import add, mul, sub, div
+
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    ad = add(a, b)
+    print("{} + {} = {}".format(a, b, ad)
+    su = sub(a, b)
+    print("{} - {} = {}".format(a, b, su)
+    mu = mul(a, b)
+    print("{} * {} = {}".format(a, b, mu)
+    di = div(a, b
+    print("{} / {} = {}".format(a, b, div)
