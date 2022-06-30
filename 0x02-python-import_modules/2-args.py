15b001fac10619f944424e1e22211c6bff21afb7
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-06-30 16:52:58 +0100

    ...

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     if arg_count < 1:
         print("{} arguments.".format(arg_count))
     else:
-        print("{} arguments:".format(arg_count))
+        print("{} argument{}:".format(arg_count, "s" if arg_count > 1 else ""))
         for i in range(arg_count):
             print("{}: {}".format(i+1, sys.argv[i+1]))
