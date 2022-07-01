6c9028e1241797eadb4007f4403aad2a25dc1261
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-02 00:08:03 +0100

    Updated repo

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for item in matrix:
+        for i in item:
+            print("{}".format(i), end=" " if item.index(i) < (len(item)-1) else "")
+        print()
