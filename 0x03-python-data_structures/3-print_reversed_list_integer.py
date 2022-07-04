f9467899fac9114f8827c66bd14ebcc00246c93a
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-04 21:30:11 +0100

    [mod] task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     _len = len(my_list)
-    if not my_list:
+    if not my_list or _len < 1:
         pass
     else:
         my_list.reverse()
