ca864e63ec0d698ac3fccaa6133e919c11b538a3
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-06 22:36:29 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqr_mtx = [[i[0]**2,i[1]**2,i[2]**2] for i in matrix]
+    sqr_mtx = [[i[0]**2, i[1]**2, i[2]**2] for i in matrix]
     return sqr_mtx
