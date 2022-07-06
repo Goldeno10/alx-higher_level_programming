a421b61f7978be6f65451f72f10e7bd3c951f770
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-06 22:30:50 +0100

    task 102

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary[key]:
-        del a_dictionary[key]
-    return a_dictionary
+    for i in list(a_dictionary):
+        if i == key:
+            del a_dictionary[i]
+    return(a_dictionary)
