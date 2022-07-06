56a709cea8ef25b9fe43e6b328d267d27dbdfab0
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-06 21:03:20 +0100

    Added task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) < 1:
+        return 0
     weight = sum([it[0] * it[1] for it in my_list])
     num = sum([it[1] for it in my_list])
     return weight / num
