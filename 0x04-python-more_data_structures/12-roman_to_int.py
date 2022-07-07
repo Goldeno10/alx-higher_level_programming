37b0f3c4510cf3a87a222940facaba368b615acd
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-07 02:25:52 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,15 @@ def roman_to_int(roman_string):
     rmn = {"I": 1, "V": 5, "X": 10,
            "L": 50, "C": 100,
            "D": 500, "M": 1000}
-
+    keys = list(rmn)
     Lst = list(roman_string)
+
+    if roman_string is None:
+        return 0
+    for i in Lst:
+        if i not in keys:
+            return 0
+
     n_L = list(map(lambda x: rmn[x], Lst))
     for i in range(len(n_L)):
         if i > 0:
