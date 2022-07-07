9a407f975571b3817b8601caee75d5b7cedfd57b
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-07 02:12:39 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-3999 =MMMCMXCIX
+    rmn = {"I": 1, "V": 5, "X": 10,
+           "L": 50, "C": 100,
+           "D": 500, "M": 1000}
+
+    Lst = list(roman_string)
+    num = 0
+    n_L = list(map(lambda x:rmn[x], Lst))
+    for  i in range(len(n_L)):
+        if i > 0:
+            if n_L[i] > n_L[i-1]:
+               if i+1 != None:
+                    n_L[i-1:i+1] = [n_L[i] - n_L[i-1]]
+                    n_L.append(0)
+               else:
+                   n_L[i-1: ] = [n_L[i] - n_L[i-1]]
+    return sum(n_L)
---
+++ 0x04-python-more_data_structures/byte.h
@@ -0,0 +1,7 @@
+#ifndef BYTE_H
+#define BYTE_H
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+#endif /* BYTE_H */
