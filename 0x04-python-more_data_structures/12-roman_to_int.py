794e45ad6781bf484bdab7d8fe9210ae2e1d1813
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-07 02:19:23 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,14 +5,13 @@ def roman_to_int(roman_string):
            "D": 500, "M": 1000}
 
     Lst = list(roman_string)
-    num = 0
-    n_L = list(map(lambda x:rmn[x], Lst))
-    for  i in range(len(n_L)):
+    n_L = list(map(lambda x: rmn[x], Lst))
+    for i in range(len(n_L)):
         if i > 0:
             if n_L[i] > n_L[i-1]:
-               if i+1 != None:
+                if i+1:
                     n_L[i-1:i+1] = [n_L[i] - n_L[i-1]]
                     n_L.append(0)
-               else:
-                   n_L[i-1: ] = [n_L[i] - n_L[i-1]]
+                else:
+                    n_L[i-1:] = [n_L[i] - n_L[i-1]]
     return sum(n_L)
