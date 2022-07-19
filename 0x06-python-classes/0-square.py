9863b41834fc81903d79cfef4cc30ef591e4bdc7
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-19 19:46:02 +0100

    ...

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+""" dusjsidjfhha sudjahdhd suufufh
+ehdhfiisg fudus djfhfh
+hfh dkfj: eidifg
+"""
+
 class Square:
     """Class Sqaure, it defines a square.
     """
