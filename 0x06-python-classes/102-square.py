39face030f2eefa15f2a871110b34713aa3fe111
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-19 23:46:22 +0100

    Bulk commits

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Class square, it defines and compute square
+properties
+"""
+
+
+class Square:
+    """Defines a square with private field 'size'
+
+    Attributes:
+        __size (int): size of a side of the square
+
+    """
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __init__(self, size=0):
+        """ __init__ initialises the inputed parameters
+
+        Args:
+           size (int): The size of the sqaure
+
+        """
+        if (type(size) == int) and (size >= 0):
+            self.__size = size
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Computes the area of a square
+
+        Returns:
+           Area of square
+        """
+        return(self.__size**2)
+
+    @property
+    def size(self):
+        """Getter for size attribute
+
+        Returns:
+           size atrribute
+        """
+        return(self.__size)
+
+    @size.setter
+    def size(self, size):
+        """Setter for size attribute
+
+        Args:
+            __size : New value for size attribute
+
+        Returns:
+            Nothing
+        """
+        if (type(size) == int) and (size >= 0):
+            self.__size = size
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
