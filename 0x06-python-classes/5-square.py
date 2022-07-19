acab2fe6120b2aceced42e1e04fd38861fc48f76
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-19 21:11:41 +0100

    Task 5 commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Class square, it defines and compute square
+properties
+"""
+
+
+class Square:
+    """Defines a square with private field 'size'
+
+    Attributes:
+        __size (int): size of a side of the square
+
+    """
+
+    def __init__(self, size=0):
+        """ __init__ initialises the inputed parameters
+
+        Args:
+           size (int): The size of the sqaure
+
+        """
+        if (type(size) == int) and (size >= 0):
+            self.__size = size
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Computes the area of a square
+
+        Returns:
+           Area of square
+        """
+        return(self.__size**2)
+
+    @property
+    def size(self):
+        """Getter for size attribute
+
+        Returns:
+           size atrribute
+        """
+        return(self.__size)
+
+    @size.setter
+    def size(self, size):
+        """Setter for size attribute
+
+        Args:
+            __size : New value for size attribute
+
+        Returns:
+            Nothing
+        """
+        if (type(size) == int) and (size >= 0):
+            self.__size = size
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        """prints in stdout the square with the 
+        character #.
+        if size is equal to 0, print an empty line
+
+        Returns:
+           Nothing
+        """
+
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
