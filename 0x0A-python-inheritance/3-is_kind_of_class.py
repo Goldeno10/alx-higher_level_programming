8e17f66fcb69f410e84a3329662d396ea8c4d8a4
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-08-02 14:50:18 +0100

    ...

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -9,5 +9,4 @@ def is_kind_of_class(obj, a_class):
     inherited from, the specified class
     otherwise False.
     """
-
-    return (isinstance(type(obj), a_class))
+    return (isinstance(obj, a_class))
