33a49f95a984e868bfbcf6a2152661e9698d3d4c
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-08-02 14:58:24 +0100

    [fix] pycodestyle and shibange

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,11 @@
-#/usr/bin/python3
-"""This Module 0-lookup.py contains only one 
+#!/usr/bin/python3
+"""This Module 0-lookup.py contains only one
 function lookup(obj)
 """
 
 
 def lookup(obj):
-    """a function that returns the list of available 
+    """a function that returns the list of available
     attributes and methods of an object
     """
-    
     return (dir(obj))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 
 def is_same_class(obj, a_class):
-    """returns True if the object is exactly an 
+    """returns True if the object is exactly an
     instance of the specified clas
     otherwise False """
     return (type(obj) == a_class)
