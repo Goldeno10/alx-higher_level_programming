27593326dbb7a4ce347800de3d0d17ead19b1248
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-08-03 14:13:40 +0100

    fixed 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,11 +11,15 @@ import os.path
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+filename = "add_item.json"
 
-save_file(my_list, "add_item.json")
+try:
+    json_list = load_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_file(json_list, filename)
