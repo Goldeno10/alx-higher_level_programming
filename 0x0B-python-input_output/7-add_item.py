19e0320a4cb1153748ee9733b7b4a28dd318f35d
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-08-05 08:55:21 +0100

    updated 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list,
-and then
-save them to a file
+"""Module that adds all arguments to a Python list,
+and then save them to a file
 """
 
 
