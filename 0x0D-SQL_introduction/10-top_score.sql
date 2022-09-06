bde83b646abe898fbde7e5ef56d344c63775b4d0
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-09-06 23:17:55 +0100

    Bulk commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table 'second_table' of the database hbtn_0c_0 in
+-- the MySQL server.
+-- Results will display both the score and the name (in this order)
+-- Records will be ordered by score (top first)
+SELECT score, name FROM second_table
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+--  Lists all records with a score >= 10 in the table 'second_table' of the
+-- database hbtn_0c_0 in the MySQL server.
+-- Results will display both the score and the name (in this order)
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
