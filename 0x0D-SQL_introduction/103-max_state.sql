a35c905840f019cd644bb8ccbbbad9a1b0f4bd46
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-09-07 11:28:13 +0100

    updated project

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
 --      $ mysql -u root -p hbtn_0c_0 < temperatures.sql
 -- Script that displays the max temperature of each state (ordered by State name).
-SELECT state, MAX(value) AS avg_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state ASC
