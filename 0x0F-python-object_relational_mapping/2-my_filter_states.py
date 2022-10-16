1b3e36b20eb5e4ad5716e60807ca57c6f4f55b11
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-10-16 11:16:50 +0100

    ..

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                 WHERE name = '{}'
+                 WHERE name = @'{}'
                  ORDER BY id ASC;""".format(sys.argv[4]))
     states = cur.fetchall()
 
