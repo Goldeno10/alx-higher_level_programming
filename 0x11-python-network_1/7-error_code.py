9382f58c741a69cdbacac29247974983527f2ea1
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-22 16:52:44 +0100

    ...

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as res:
+        if res.status_code < 400:
+            print(res.text)
+        else:
+            print(f'Error code: {res.status_code}')
