5e79864935c4f1a3d1de3e5ea70c3144da86fdf1
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-22 19:26:39 +0100

    Added the "10. Time for an interview!" task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Contains script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    res = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+    dict_list = res.json()
+    for item in dict_list:
+        sha = item.get('sha')
+        author = item.get('commit').get('author').get('name')
+        print(f'{sha}: {author}')
