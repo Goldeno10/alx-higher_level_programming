8a9eace42e2f40dd3f915ab7d6ec40cc56077995
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-22 15:20:36 +0100

    ...

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -4,4 +4,4 @@
 # Your script must send a POST request with the contents of a file,passed
 #+ with the filename as the second argument of the script, in
 #+ the body of the request
-curl -sX Post $1 -H 'Content-Type: application/json' -d $2
+curl -sX Post "$1" -H 'Content-Type: application/json' -d "$2"
