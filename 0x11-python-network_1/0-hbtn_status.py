2bc7d02288cb5fd6f1d7988aadcc10e3a2368c6a
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-22 19:40:15 +0100

    Added the "10. Time for an interview!" task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(url) as response:
         page = response.read()
-    print('Body response:')
-    print('    - type: ', type(page))
-    print('    - content: ', page)
-    print('    - utf8 content: ', page.decode('utf8'))
+        print('Body response:')
+        print('    - type: ', type(page))
+        print('    - content: ', page)
+        print('    - utf8 content: ', page.decode('utf8'))
