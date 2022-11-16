454997882687ceb7601fe628891f5cea0be8f517
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-16 13:03:17 +0000

    updated repo

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const fs = require('fs');
+const filename = process.argv.slice(2)[0];
+const text = process.argv.slice(2)[1];
+if (filename && text) {
+  fs.writeFile(filename, text, 'utf8', (e) => {
+    if (e) throw e;
+  });
+}
