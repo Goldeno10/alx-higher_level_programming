157feadab8360e593d2490a92f0f3d99fe107072
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-16 21:17:29 +0000

    Updated repo

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Scrip that display the status code of get request
 const request = require('request');
-const url = process.argv.slice(2)[0];
+const url = process.argv[2];
 if (url){
-	request.get(url, (e, res) => {
+	request(url, (e, res) => {
 		if (!err){
 		consol.log('code: ' + res && res.statuscode);
 		}
