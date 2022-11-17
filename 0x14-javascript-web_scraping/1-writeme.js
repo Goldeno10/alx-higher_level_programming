be673643cc6f0e4fe8bd1fbf4aeb41de3e55cf09
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-17 00:16:46 +0000

    Updated repo

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
-my_file.txt
+#!/usr/bin/node
+// script that writes a string to a file.
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', (err) => {
+  if (err) console.log(err);
+});
